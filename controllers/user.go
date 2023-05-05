f59da1bb25bddfe202ed86ed5e08d9b02aeee14d
Alan Njogu <bigboydevelops@gmail.com>
2023-05-05 17:38:16 +0300

    add: API functions

--- controllers/user.go
+++ controllers/user.go
@@ -1,45 +1,110 @@
+/*
+This file contains logic for various API functions
+*/
+
 package controllers
 
 import (
 	"net/http"
+	"api/utils/token"
 	models "api/models"
 	"github.com/gin-gonic/gin"
 )
 
 
-// POST user
 // create a new user
-func AddUser(c *gin.Context){
-
-	db, err := models.ConnectDB()
-
-	if err != nil {
-		panic(err)
-	}
+func CreateAccount(c *gin.Context){
 	
 	// Validate Input
-	// var input models.CreateUserInput
-	// if err := c.BindJSON(&input); err != nil {
-	// 	c.IndentedJSON(http.StatusBadRequest, gin.H{
-	// 		"error" : err.Error(),
-	// 	})
-	// 	return 
-	// }
+	var input models.CreateUserInput
 
-	// Create User
-	sqlStatement := `INSERT INTO Users (
-		username, firstname, lastname, gender, email, password, phone_number
-		) VALUES ($1, $2, $3, $4, $5, $6, $7)`
-    _, err = db.Exec(sqlStatement, "Njoguu", "Alan", "Njogu","male", "smith@acme.com", "alannjoguu", "+09088789")
-    if err != nil {
+	if err := c.ShouldBindJSON(&input); err != nil{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// // Create User
+	user := models.User{}
+	
+	user.Username = input.Username
+	user.FirstName = input.FirstName
+	user.LastName = input.LastName
+	user.Gender = input.Gender
+	user.EmailAddress = input.EmailAddress
+	user.Password = input.Password
+	user.City = input.City
+	user.PhoneNumber = input.PhoneNumber
+
+	_,err := user.SaveUser()
+
+	if err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{
 			"error" : err.Error(),
 		})
-		panic(err)
-    } else {
-        c.IndentedJSON(http.StatusCreated, gin.H{
-			"message": "User Created!",
-		})
+		return
     }
-	return
+	c.IndentedJSON(http.StatusCreated, gin.H{
+		"message": "User Created!",
+	})
+}
+
+// login user
+func Login(c *gin.Context){
+
+	// Validate input
+	var input models.LoginInput
+	if err := c.ShouldBindJSON(&input); err != nil{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Login User
+	user := models.User{}
+	user.Username = input.Username
+	user.Password = input.Password
+
+	token, err := models.LoginCheck(user.Username, user.Password)
+
+	if err != nil{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"error": "Username or Password is incorrect",
+		})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"message": "Login Success",
+		"token": token,
+	})
+}
+
+// get current user
+func GetCurrentUser(c *gin.Context){
+
+	uid, err := token.ExtractTokenID(c)
+
+	if err != nil{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	user,err := models.GetUser(uid)
+
+	if err != nil{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"message": "Success!",
+		"data": user,
+	})
 }
