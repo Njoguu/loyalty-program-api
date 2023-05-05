d4f4d5db12cfdfe2db9e03a4ad79a9a5d08373f6
Alan Njogu <bigboydevelops@gmail.com>
2023-05-05 17:44:49 +0300

    add & update: public and protected routes

--- main.go
+++ main.go
@@ -3,32 +3,37 @@ package main
 import (
 	"net/http"
 	"api/models"
+	"api/middlewares"
 	"github.com/gin-gonic/gin"
 	controllers "api/controllers"
 )
 
 
 func main(){
+
+	// Initialzie DB Connection
+	models.ConnectDB() 
+	
 	r := gin.Default() //router
 
-	db,err := models.ConnectDB() 
-	if err != nil{
-		panic(err)
-	}
-	// Provide db variable to controllers
-	r.Use(func(c *gin.Context) {
-		c.Set("db", db)
-		c.Next()
-	})
+	// Group endpoints
+	public := r.Group("/api")
+	protected := r.Group("/api/user")
 
-	// Test purposes
+	// Test API works
 	r.GET("/ping", func(c *gin.Context){
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.POST("/user", controllers.AddUser)
+	// Public routes
+	public.POST("/register", controllers.CreateAccount)
+	public.POST("/login", controllers.Login)
+
+	// Protected routes
+	protected.Use(middlewares.JwtAuthMiddleware())
+	protected.GET("/me", controllers.GetCurrentUser)
 
 	r.Run() // listen and serve on 0.0.0.0:8080	
 }
