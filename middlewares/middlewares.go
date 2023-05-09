b55b997e2376285dd547b6a5e3fa934781c78d1a
Alan Njogu <bigboydevelops@gmail.com>
2023-05-09 20:52:48 +0300

    rewrite: token auth

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -1,31 +1,60 @@
-/*
-This Package contains middleware functions
-*/
-
-package middlewares
+package middleware
 
 import (
+	"api/models"
+	tokens "api/utils/token"
+	"fmt"
+	"log"
 	"net/http"
-	"api/utils/token"
+	"os"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-/*
-JwtAuthMiddleware is a middleware function that ensures JWT token 
-authentication for incoming requests.
-It returns a gin.HandlerFunc that can be added to the middleware stack 
-to protect routes from unauthorized access.
-*/
-func JwtAuthMiddleware() gin.HandlerFunc{
+func DeserializeUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var token string
+		cookie, err := c.Cookie("token")
 
-	// Return a new handler function that takes in a Gin context
-	//  and checks for a valid JWT token.
-	return func(c *gin.Context){
-		err := token.TokenValid(c)
-		if err != nil {
-			c.IndentedJSON(http.StatusUnauthorized, gin.H{
-				"message": "Unauthorized!",
-			})
+		authorizationHeader := c.Request.Header.Get("Authorization")
+		fields := strings.Fields(authorizationHeader)
+
+		if len(fields) != 0 && fields[0] == "Bearer" {
+			token = fields[1]
+		} else if err == nil {
+			token = cookie
 		}
+
+		if token == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "fail", "message": "You are not logged in"})
+			return
+		}
+
+		// Load .env file
+		errr := godotenv.Load(".env")
+		if errr != nil {
+			log.Fatal("Error loading .env file")
+		}
+
+		sub, err := tokens.ValidateToken(token, os.Getenv("TOKEN_SECRET"))
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "fail", "message": err.Error()})
+			return
+		}
+
+		var user models.User
+		result := models.DB.First(&user, "id = ?", fmt.Sprint(sub))
+		if result.Error != nil {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+				"status": "fail",
+				"message": "the user belonging to this token no logger exists",
+			})
+			return
+		}
+
+		c.Set("currentUser", user)
+		c.Next()
 	}
 }
